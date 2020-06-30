5d27b51fbd4a79f0969393c26800cb82d0377ce5
James Bale <james@buzzinteractive.co.uk>
2020-06-30 21:12:57 +0100

    Added auto hiding gallery controls

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,15 +1,27 @@
 import React from "react"
 import Img from "gatsby-image/withIEPolyfill"
 import ReactImageGallery from 'react-image-gallery';
-import { Fab } from "@material-ui/core";
+import { IconButton, Box } from "@material-ui/core";
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRight from '@material-ui/icons/ChevronRight';
+import Fullscreen from '@material-ui/icons/Fullscreen';
+import FullscreenExit from '@material-ui/icons/FullscreenExit';
 import { makeStyles } from "@material-ui/styles";
 import PropTypes from 'prop-types';
 import useMouseActive from "../hooks/useMouseActive";
+import { withDarkTheme } from "../theme";
+
+const navBase = {
+  position: 'absolute',
+  zIndex: 4,
+  transition: '0.5s'
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    '& .image-gallery-slide-wrapper': {
+      overflow: 'hidden'
+    },
     '& .image-gallery-content.fullscreen': {
       height: '100vh',
       display: 'flex',
@@ -35,18 +47,33 @@ const useStyles = makeStyles((theme) => ({
     }
   },
   leftNav: {
-    position: 'absolute',
+    ...navBase,
     left: theme.spacing(2),
     top: '50%',
     transform: 'translateY(-50%)',
-    zIndex: 4
+    '&.hidden': {
+      transform: 'translate(-100%, -50%)'
+    }
   },
   rightNav: {
-    position: 'absolute',
+    ...navBase,
     right: theme.spacing(2),
     top: '50%',
     transform: 'translateY(-50%)',
-    zIndex: 4
+    '&.hidden': {
+      transform: 'translate(100%, -50%)'
+    }
+  },
+  fullscreenNav: {
+    ...navBase,
+    right: theme.spacing(2),
+    bottom: theme.spacing(2),
+    '&.hidden': {
+      transform: 'scale(0)'
+    }
+  },
+  icon: {
+    filter: 'drop-shadow(0 2px 2px #1a1a1a)'
   }
 }));
 
@@ -65,20 +92,30 @@ const ImageGallery = ({images}) => {
 
   const renderLeftNav = (onClick, disabled) => {
     return (
-      <div className={classes.leftNav}>
-        <Fab aria-label="previous" disabled={disabled} onClick={onClick} size="small">
-          <ChevronLeftIcon />
-        </Fab>
+      <div className={`${classes.leftNav} ${mouseActive ? '' : 'hidden'}`}>
+        <IconButton aria-label="previous" disabled={disabled} onClick={onClick} size="medium">
+          <ChevronLeftIcon className={classes.icon} fontSize="large" />
+        </IconButton>
       </div>
     )
   }
 
   const renderRightNav = (onClick, disabled) => {
     return (
-      <div className={classes.rightNav}>
-        <Fab aria-label="next" disabled={disabled} onClick={onClick} size="small">
-          <ChevronRight />
-        </Fab>
+      <div className={`${classes.rightNav} ${mouseActive ? '' : 'hidden'}`}>
+        <IconButton aria-label="next" disabled={disabled} onClick={onClick} size="medium">
+          <ChevronRight className={classes.icon} fontSize="large" />
+        </IconButton>
+      </div>
+    )
+  }
+
+  const renderFullscreenButton = (onClick, isFullscreen) => {
+    return (
+      <div className={`${classes.fullscreenNav} ${mouseActive ? '' : 'hidden'}`}>
+        <IconButton aria-label="fullscreen" onClick={onClick} size="medium">
+          { isFullscreen ? <FullscreenExit className={classes.icon} fontSize="large" /> : <Fullscreen className={classes.icon} fontSize="large" /> }
+        </IconButton>
       </div>
     )
   }
@@ -107,6 +144,7 @@ const ImageGallery = ({images}) => {
         renderItem={renderImage}
         renderLeftNav={renderLeftNav}
         renderRightNav={renderRightNav}
+        renderFullscreenButton={renderFullscreenButton}
         onScreenChange={onScreenChange}
         />
     </div>
@@ -124,4 +162,4 @@ ImageGallery.propTypes = {
   )
 }
 
-export default ImageGallery
+export default withDarkTheme(ImageGallery)
